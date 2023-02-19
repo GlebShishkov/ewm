4d574aa67f218766967d276688d4e4f4c44db9e6
Gleb Shishkov <gleb.shishkov8@gmail.com>
2023-02-19 18:28:02 +0300

    Added skeleton

---
+++ main-service/src/main/java/ru/practicum/MainService.java
@@ -0,0 +1,17 @@
+package ru.practicum;
+
+import java.util.Map;
+
+public class MainService {
+    public static void main(String[] args) {
+        StatsClient statsClient = new StatsClient();
+
+        StatsDto statsDto = new StatsDto();
+
+//      findByIdAndStatus....
+
+//      Querydsl,  JPA Specifications
+
+    }
+
+}
---
+++ stats-service/stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -0,0 +1,8 @@
+package ru.practicum;
+
+public class StatsClient {
+
+    public static void main(String[] args) {
+        StatsDto statsDto = new StatsDto();
+    }
+}
